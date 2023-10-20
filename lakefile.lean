d4b3c303bee2a9d58fd76877981ccede5f85f510
matematiflo <7537413+matematiflo@users.noreply.github.com>
2023-10-20 03:29:36 +0200

    Basic setup

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package «test» where
   -- add package configuration options here
 
-lean_lib «Foo» where
+lean_lib «test» where
   -- add library configuration options here
   require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
