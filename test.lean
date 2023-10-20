a4410f8fe49b83343071e0137636e60271d7968a
Florent Schaffhauser <7537413+matematiflo@users.noreply.github.com>
2023-10-20 19:07:32 +0000

    Test branch

--- test.lean
+++ test.lean
@@ -5,3 +5,5 @@ example (G : Type) [Group G] : 1 * 1 = 1 := by {rfl}
 example : 1 + 1 = 2 := refl 2
 example : 1 + 1 = 2 := refl (1 +1)
 example : 1 + 1 = 2 := rfl  -- same as `refl 2`, with the argument taken implicitly
+
+example : 1 = 1 := by {exact refl 1}
